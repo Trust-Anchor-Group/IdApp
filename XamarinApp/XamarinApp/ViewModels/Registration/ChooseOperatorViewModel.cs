977ab9bce9c90a126840fbcf0c2cda4f949b9141
Henrik Oscarsson <henrik@capsor.se>
2020-11-26 14:02:07 +0100

    Fixed pesky compiler errors.

--- XamarinApp/XamarinApp/ViewModels/Registration/ChooseOperatorViewModel.cs
+++ XamarinApp/XamarinApp/ViewModels/Registration/ChooseOperatorViewModel.cs
@@ -24,7 +24,6 @@ namespace XamarinApp.ViewModels.Registration
             this.ManualOperatorCommand = new Command<string>(async text => await ManualOperatorTextEdited(text));
             this.PopulateOperators();
             this.Title = AppResources.SelectOperator;
-            this.InviteReviewerCommand = new Command(async () => await InviteReviewer());
         }
 
         public ObservableCollection<string> Operators { get; }
