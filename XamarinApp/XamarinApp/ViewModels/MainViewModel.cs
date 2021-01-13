b7a92129c471d3e441f95fc73e4bbfbaf810aae9
Henrik Oscarsson <henrik@capsor.se>
2021-01-13 13:20:58 +0100

    Added a base class view model that monitors connected state.

--- XamarinApp/XamarinApp/ViewModels/MainViewModel.cs
+++ XamarinApp/XamarinApp/ViewModels/MainViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Tag.Sdk.Core;
 using Tag.Sdk.Core.Services;
-using Tag.Sdk.UI.ViewModels;
 using Waher.Networking.XMPP;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -9,57 +8,32 @@ using XamarinApp.Extensions;
 
 namespace XamarinApp.ViewModels
 {
-    public class MainViewModel : BaseViewModel
+    public class MainViewModel : NeuronViewModel
     {
         private readonly ITagProfile tagProfile;
-        private readonly INeuronService neuronService;
         private readonly INetworkService networkService;
-        private readonly IUiDispatcher uiDispatcher;
 
         public MainViewModel()
+            : base(DependencyService.Resolve<INeuronService>(), DependencyService.Resolve<IUiDispatcher>())
         {
             this.tagProfile = DependencyService.Resolve<ITagProfile>();
-            this.neuronService = DependencyService.Resolve<INeuronService>();
             this.networkService = DependencyService.Resolve<INetworkService>();
-            this.uiDispatcher = DependencyService.Resolve<IUiDispatcher>();
-            this.ConnectionStateText = AppResources.XmppState_Offline;
         }
 
         protected override async Task DoBind()
         {
             await base.DoBind();
             this.IsOnline = this.networkService.IsOnline;
-            this.SetConnectionStateText(this.neuronService.State);
-            this.neuronService.ConnectionStateChanged += NeuronService_ConnectionStateChanged;
             this.networkService.ConnectivityChanged += NetworkService_ConnectivityChanged;
         }
 
         protected override Task DoUnbind()
         {
-            this.neuronService.ConnectionStateChanged -= NeuronService_ConnectionStateChanged;
             this.networkService.ConnectivityChanged -= NetworkService_ConnectivityChanged;
             return base.DoUnbind();
         }
 
         #region Properties
-
-        public static readonly BindableProperty ConnectionStateTextProperty =
-            BindableProperty.Create("ConnectionStateText", typeof(string), typeof(MainViewModel), default(string));
-
-        public string ConnectionStateText
-        {
-            get { return (string)GetValue(ConnectionStateTextProperty); }
-            set { SetValue(ConnectionStateTextProperty, value); }
-        }
-
-        public static readonly BindableProperty IsConnectedProperty =
-            BindableProperty.Create("IsConnected", typeof(bool), typeof(MainViewModel), default(bool));
-
-        public bool IsConnected
-        {
-            get { return (bool)GetValue(IsConnectedProperty); }
-            set { SetValue(IsConnectedProperty, value); }
-        }
 
         public static readonly BindableProperty IsOnlineProperty =
             BindableProperty.Create("IsOnline", typeof(bool), typeof(MainViewModel), default(bool));
@@ -72,17 +46,12 @@ namespace XamarinApp.ViewModels
 
         #endregion
 
-        public void NeuronService_ConnectionStateChanged(object sender, ConnectionStateChangedEventArgs e)
-        {
-            this.uiDispatcher.BeginInvokeOnMainThread(() => this.SetConnectionStateText(e.State));
-        }
-
         private void NetworkService_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
         {
-            this.uiDispatcher.BeginInvokeOnMainThread(() => this.IsOnline = this.networkService.IsOnline);
+            this.UiDispatcher.BeginInvokeOnMainThread(() => this.IsOnline = this.networkService.IsOnline);
         }
 
-        private void SetConnectionStateText(XmppState state)
+        protected override void SetConnectionStateText(XmppState state)
         {
             this.ConnectionStateText = state.ToDisplayText(this.tagProfile.Domain);
             this.IsConnected = state == XmppState.Connected;
