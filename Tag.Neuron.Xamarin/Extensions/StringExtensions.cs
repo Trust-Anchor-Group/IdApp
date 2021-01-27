b04417275f36781ec24984540aae70c1ed1d93bc
Henrik Oscarsson <henrik@capsor.se>
2021-01-27 08:02:19 +0100

    Added helper method in the form of an extension.

---
+++ Tag.Neuron.Xamarin/Extensions/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Tag.Neuron.Xamarin.Extensions
+{
+    public static class StringExtensions
+    {
+        private static readonly List<string> invalidFileNameChars;
+        private static readonly List<string> invalidPathChars;
+
+        static StringExtensions()
+        {
+            invalidFileNameChars = Path.GetInvalidFileNameChars().Select(x => x.ToString()).ToList();
+            invalidPathChars = Path.GetInvalidPathChars().Select(x => x.ToString()).ToList();
+        }
+
+        public static string ToSafeFileName(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return string.Empty;
+
+            str = str.Trim().Replace(" ", string.Empty);
+
+            foreach (string s in invalidFileNameChars)
+            {
+                str = str.Replace(s, string.Empty);
+            }
+
+            foreach (string s in invalidPathChars)
+            {
+                str = str.Replace(s, string.Empty);
+            }
+
+            return str;
+        }
+    }
+}
