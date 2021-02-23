e2413cc294339060e4bbe42cd70afd91c369aaef
Peter Waher <peterwaher@hotmail.com>
2021-02-23 13:33:45 +0100

    Fix profiling Register()

--- Tag.Neuron.Xamarin/Services/StorageService.cs
+++ Tag.Neuron.Xamarin/Services/StorageService.cs
@@ -12,222 +12,226 @@ using Waher.Runtime.Profiling;
 
 namespace Tag.Neuron.Xamarin.Services
 {
-    [Singleton]
-    internal sealed class StorageService : IStorageService
-    {
-        private FilesProvider databaseProvider;
-        private readonly ILogService logService;
-        private readonly ICryptoService cryptoService;
-        private readonly IUiDispatcher uiDispatcher;
-        private bool isInitializing;
-        private StorageState? initializedState;
-        private TaskCompletionSource<StorageState> databaseTcs;
-        private readonly string appDataFolder;
-        private readonly string dataFolder;
+	[Singleton]
+	internal sealed class StorageService : IStorageService
+	{
+		private FilesProvider databaseProvider;
+		private readonly ILogService logService;
+		private readonly ICryptoService cryptoService;
+		private readonly IUiDispatcher uiDispatcher;
+		private bool isInitializing;
+		private StorageState? initializedState;
+		private TaskCompletionSource<StorageState> databaseTcs;
+		private readonly string appDataFolder;
+		private readonly string dataFolder;
 
-        /// <summary>
-        /// Creates a new instance of the <see cref="StorageService"/> class.
-        /// </summary>
-        /// <param name="logService">The log service to use for logging.</param>
-        /// <param name="cryptoService">The crypto service to use.</param>
-        /// <param name="uiDispatcher">The UI Dispatcher, for main thread access and to display alerts.</param>
-        public StorageService(ILogService logService, ICryptoService cryptoService, IUiDispatcher uiDispatcher)
-        {
-            this.logService = logService;
-            this.cryptoService = cryptoService;
-            this.uiDispatcher = uiDispatcher;
-            this.appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            this.dataFolder = Path.Combine(appDataFolder, "Data");
-        }
+		/// <summary>
+		/// Creates a new instance of the <see cref="StorageService"/> class.
+		/// </summary>
+		/// <param name="logService">The log service to use for logging.</param>
+		/// <param name="cryptoService">The crypto service to use.</param>
+		/// <param name="uiDispatcher">The UI Dispatcher, for main thread access and to display alerts.</param>
+		public StorageService(ILogService logService, ICryptoService cryptoService, IUiDispatcher uiDispatcher)
+		{
+			this.logService = logService;
+			this.cryptoService = cryptoService;
+			this.uiDispatcher = uiDispatcher;
+			this.appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+			this.dataFolder = Path.Combine(appDataFolder, "Data");
+		}
 
-        #region LifeCycle management
+		#region LifeCycle management
 
-        /// <inheritdoc />
-        public void Init(ProfilerThread Thread)
-        {
-            if (this.isInitializing)
-                return;
-            
-            if (this.initializedState != null)
-                return;
+		/// <inheritdoc />
+		public void Init(ProfilerThread Thread)
+		{
+			if (this.isInitializing)
+				return;
 
-            this.databaseTcs = new TaskCompletionSource<StorageState>();
-            this.isInitializing = true;
-            _ = InitializeAsync(Thread);
-        }
+			if (this.initializedState != null)
+				return;
 
-        /// <inheritdoc />
-        public Task<StorageState> WaitForReadyState()
-        {
-            if (this.databaseTcs != null)
-            {
-                return this.databaseTcs.Task;
-            }
+			this.databaseTcs = new TaskCompletionSource<StorageState>();
+			this.isInitializing = true;
+			_ = InitializeAsync(Thread);
+		}
 
-            return Task.FromResult(this.initializedState.GetValueOrDefault());
-        }
+		/// <inheritdoc />
+		public Task<StorageState> WaitForReadyState()
+		{
+			if (this.databaseTcs != null)
+			{
+				return this.databaseTcs.Task;
+			}
 
-        /// <inheritdoc />
-        public async Task TryRepairDatabase(ProfilerThread Thread)
-        {
-            if (await this.uiDispatcher.DisplayAlert(AppResources.DatabaseIssue, AppResources.DatabaseCorruptInfoText, AppResources.RepairAndContinue, AppResources.ContinueAnyway))
-            {
-                string method = "Delete database file(s) and create new empty database";
-                try
-                {
-                    Thread?.NewState("Delete");
+			return Task.FromResult(this.initializedState.GetValueOrDefault());
+		}
 
-                    // 3. Delete and create a new empty database
-                    Directory.Delete(dataFolder, true);
+		/// <inheritdoc />
+		public async Task TryRepairDatabase(ProfilerThread Thread)
+		{
+			if (await this.uiDispatcher.DisplayAlert(AppResources.DatabaseIssue, AppResources.DatabaseCorruptInfoText, AppResources.RepairAndContinue, AppResources.ContinueAnyway))
+			{
+				string method = "Delete database file(s) and create new empty database";
+				try
+				{
+					Thread?.NewState("Delete");
 
-                    Thread?.NewState("Recreate");
+					// 3. Delete and create a new empty database
+					Directory.Delete(dataFolder, true);
 
-                    this.databaseProvider = await CreateDatabaseFile();
+					Thread?.NewState("Recreate");
 
-                    Thread?.NewState("Repair3");
+					this.databaseProvider = await CreateDatabaseFile();
 
-                    await this.databaseProvider.RepairIfInproperShutdown(string.Empty);
-                }
-                catch (Exception e3)
-                {
-                    e3 = Log.UnnestException(e3);
-                    Thread?.Exception(e3);
+					Thread?.NewState("Repair3");
 
-                    // Delete and create new failed. We're out of options.
-                    this.logService.LogException(e3, this.GetClassAndMethod(MethodBase.GetCurrentMethod(), method));
+					await this.databaseProvider.RepairIfInproperShutdown(string.Empty);
+				}
+				catch (Exception e3)
+				{
+					e3 = Log.UnnestException(e3);
+					Thread?.Exception(e3);
 
-                    Thread?.NewState("DisplayAlert2");
+					// Delete and create new failed. We're out of options.
+					this.logService.LogException(e3, this.GetClassAndMethod(MethodBase.GetCurrentMethod(), method));
 
-                    await this.uiDispatcher.DisplayAlert(AppResources.DatabaseIssue, AppResources.DatabaseRepairFailedInfoText, AppResources.Ok);
-                }
-            }
-        }
+					Thread?.NewState("DisplayAlert2");
 
-        /// <inheritdoc />
-        public async Task Shutdown()
-        {
-            if (this.databaseProvider != null)
-            {
-                await this.databaseProvider.Flush();
-                this.databaseProvider.Dispose();
-                this.databaseProvider = null;
-            }
+					await this.uiDispatcher.DisplayAlert(AppResources.DatabaseIssue, AppResources.DatabaseRepairFailedInfoText, AppResources.Ok);
+				}
+			}
+		}
 
-            this.isInitializing = false;
-            this.initializedState = null;
-        }
+		/// <inheritdoc />
+		public async Task Shutdown()
+		{
+			if (this.databaseProvider != null)
+			{
+				await this.databaseProvider.Flush();
+				this.databaseProvider.Dispose();
+				this.databaseProvider = null;
+			}
 
-        private Task<FilesProvider> CreateDatabaseFile()
-        {
-            return FilesProvider.CreateAsync(dataFolder, "Default", 8192, 10000, 8192, Encoding.UTF8, (int)Constants.Timeouts.Database.TotalMilliseconds, this.cryptoService.GetCustomKey);
-        }
+			this.isInitializing = false;
+			this.initializedState = null;
+		}
 
-        private async Task InitializeAsync(ProfilerThread Thread)
-        {
-            string createDbMethod = $"{nameof(FilesProvider)}.{nameof(FilesProvider.CreateAsync)}()";
+		private Task<FilesProvider> CreateDatabaseFile()
+		{
+			return FilesProvider.CreateAsync(dataFolder, "Default", 8192, 10000, 8192, Encoding.UTF8, (int)Constants.Timeouts.Database.TotalMilliseconds, this.cryptoService.GetCustomKey);
+		}
 
-            string method = null;
-            try
-            {
-                Thread?.NewState("Provider");
+		private async Task InitializeAsync(ProfilerThread Thread)
+		{
+			string createDbMethod = $"{nameof(FilesProvider)}.{nameof(FilesProvider.CreateAsync)}()";
 
-                // 1. Try create database
-                method = createDbMethod;
-                this.databaseProvider = await CreateDatabaseFile();
+			string method = null;
+			try
+			{
+				Thread?.NewState("Provider");
 
-                Thread?.NewState("Repair");
+				// 1. Try create database
+				method = createDbMethod;
+				this.databaseProvider = await CreateDatabaseFile();
 
-                method = nameof(FilesProvider.RepairIfInproperShutdown);
-                await this.databaseProvider.RepairIfInproperShutdown(string.Empty);
-            }
-            catch (Exception e1)
-            {
-                e1 = Log.UnnestException(e1);
-                Thread?.Exception(e1);
+				Thread?.NewState("Repair");
 
-                // Create failed.
-                this.logService.LogException(e1, this.GetClassAndMethod(MethodBase.GetCurrentMethod(), method));
+				method = nameof(FilesProvider.RepairIfInproperShutdown);
+				await this.databaseProvider.RepairIfInproperShutdown(string.Empty);
+			}
+			catch (Exception e1)
+			{
+				e1 = Log.UnnestException(e1);
+				Thread?.Exception(e1);
 
-                try
-                {
-                    Thread?.NewState("Repair2");
+				// Create failed.
+				this.logService.LogException(e1, this.GetClassAndMethod(MethodBase.GetCurrentMethod(), method));
 
-                    // 2. Try repair database
-                    if (this.databaseProvider == null && Database.HasProvider)
-                    {
-                        // This is an attempt that _can_ work.
-                        // During a soft restart, there _may_ be a provider registered already. If so, grab it.
-                        this.databaseProvider = Database.Provider as FilesProvider;
-                    }
+				try
+				{
+					Thread?.NewState("Repair2");
 
-                    if (this.databaseProvider == null)
-                    {
-                        // Reasoning: If we can't create a provider, and the database doesn't have one assigned either, we're in serious trouble.
-                        // Throw an exception, which is caught below, to try and perform a recovery.
-                        const string message = "Database does not have a provider, and one cannot be created because the Database file(s) are locked. Catch 22.";
-                        method = createDbMethod;
-                        throw new InvalidOperationException(message);
-                    }
-                    method = nameof(FilesProvider.RepairIfInproperShutdown);
-                    await this.databaseProvider.RepairIfInproperShutdown(string.Empty);
-                }
-                catch (Exception e2)
-                {
-                    e2 = Log.UnnestException(e2);
-                    Thread?.Exception(e2);
+					// 2. Try repair database
+					if (this.databaseProvider == null && Database.HasProvider)
+					{
+						// This is an attempt that _can_ work.
+						// During a soft restart, there _may_ be a provider registered already. If so, grab it.
+						this.databaseProvider = Database.Provider as FilesProvider;
+					}
 
-                    // Repair failed
-                    this.logService.LogException(e2, this.GetClassAndMethod(MethodBase.GetCurrentMethod(), method));
-                }
-            }
+					if (this.databaseProvider == null)
+					{
+						// Reasoning: If we can't create a provider, and the database doesn't have one assigned either, we're in serious trouble.
+						// Throw an exception, which is caught below, to try and perform a recovery.
+						const string message = "Database does not have a provider, and one cannot be created because the Database file(s) are locked. Catch 22.";
+						method = createDbMethod;
+						throw new InvalidOperationException(message);
+					}
+					method = nameof(FilesProvider.RepairIfInproperShutdown);
+					await this.databaseProvider.RepairIfInproperShutdown(string.Empty);
+				}
+				catch (Exception e2)
+				{
+					e2 = Log.UnnestException(e2);
+					Thread?.Exception(e2);
 
-            try
-            {
-                Thread?.NewState("Register");
+					// Repair failed
+					this.logService.LogException(e2, this.GetClassAndMethod(MethodBase.GetCurrentMethod(), method));
+				}
+			}
 
-                method = $"{nameof(Database)}.{nameof(Database.Register)}";
-                Database.Register(databaseProvider, false);
-                // All is good.
-                this.SetState(StorageState.Ready);
-            }
-            catch (Exception e)
-            {
-                e = Log.UnnestException(e);
-                Thread?.Exception(e);
-                this.logService.LogException(e, this.GetClassAndMethod(MethodBase.GetCurrentMethod(), method));
-                this.SetState(StorageState.NeedsRepair);
-            }
-        }
+			try
+			{
+				Thread?.NewState("Register");
 
-        private void SetState(StorageState state)
-        {
-            this.isInitializing = false;
-            this.initializedState = state;
-            this.databaseTcs.TrySetResult(state);
-            this.databaseTcs = null;
-        }
+				method = $"{nameof(Database)}.{nameof(Database.Register)}";
+				Database.Register(databaseProvider, false);
+				// All is good.
+				this.SetState(StorageState.Ready);
+			}
+			catch (Exception e)
+			{
+				e = Log.UnnestException(e);
+				Thread?.Exception(e);
+				this.logService.LogException(e, this.GetClassAndMethod(MethodBase.GetCurrentMethod(), method));
+				this.SetState(StorageState.NeedsRepair);
+			}
+			finally
+			{
+				Thread?.Idle();
+			}
+		}
+
+		private void SetState(StorageState state)
+		{
+			this.isInitializing = false;
+			this.initializedState = state;
+			this.databaseTcs.TrySetResult(state);
+			this.databaseTcs = null;
+		}
 
 
-        #endregion
+		#endregion
 
-        public Task Insert(object obj)
-        {
-            return Database.Insert(obj);
-        }
+		public Task Insert(object obj)
+		{
+			return Database.Insert(obj);
+		}
 
-        public Task Update(object obj)
-        {
-            return Database.Update(obj);
-        }
+		public Task Update(object obj)
+		{
+			return Database.Update(obj);
+		}
 
-        public Task<T> FindFirstDeleteRest<T>() where T : class
-        {
-            return Database.FindFirstDeleteRest<T>();
-        }
+		public Task<T> FindFirstDeleteRest<T>() where T : class
+		{
+			return Database.FindFirstDeleteRest<T>();
+		}
 
-        public Task<T> FindFirstIgnoreRest<T>() where T : class
-        {
-            return Database.FindFirstIgnoreRest<T>();
-        }
-    }
+		public Task<T> FindFirstIgnoreRest<T>() where T : class
+		{
+			return Database.FindFirstIgnoreRest<T>();
+		}
+	}
 }
