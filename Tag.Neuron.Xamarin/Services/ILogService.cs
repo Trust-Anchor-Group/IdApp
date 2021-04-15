67a9ee6cd3894ca4a99b79c7d6e36c8b2f6c5c75
Peter Waher <peterwaher@hotmail.com>
2021-04-15 13:53:33 +0200

    Minor edits

--- Tag.Neuron.Xamarin/Services/ILogService.cs
+++ Tag.Neuron.Xamarin/Services/ILogService.cs
@@ -17,28 +17,33 @@ namespace Tag.Neuron.Xamarin.Services
         /// </summary>
         /// <param name="eventSink">The listener to add.</param>
         void AddListener(IEventSink eventSink);
+
         /// <summary>
         /// Removes an <see cref="IEventSink"/> to the log service.
         /// </summary>
         /// <param name="eventSink">The listener to remove.</param>
         void RemoveListener(IEventSink eventSink);
+        
         /// <summary>
         /// Invoke this method to add a warning statement to the log.
         /// </summary>
         /// <param name="format">A composite format string.</param>
         /// <param name="args">The objects to format.</param>
         void LogWarning(string format, params object[] args);
+        
         /// <summary>
         /// Invoke this method to add an <see cref="Exception"/> entry to the log.
         /// </summary>
         /// <param name="e"></param>
         void LogException(Exception e);
+        
         /// <summary>
         /// Invoke this method to add an <see cref="Exception"/> entry to the log.
         /// </summary>
         /// <param name="e">The exception to log.</param>
         /// <param name="extraParameters">Any extra parameters that are added to the log.</param>
         void LogException(Exception e, params KeyValuePair<string, string>[] extraParameters);
+        
         /// <summary>
         /// Invoke this method to add an event entry to the log.
         /// </summary>
@@ -52,11 +57,13 @@ namespace Tag.Neuron.Xamarin.Services
         /// <param name="title">The title of the stack trace.</param>
         /// <param name="stackTrace">The actual stack trace.</param>
         void SaveExceptionDump(string title, string stackTrace);
+        
         /// <summary>
         /// Restores any exception dump that has previously been persisted with the <see cref="SaveExceptionDump"/> method.
         /// </summary>
         /// <returns>The exception dump, if it exists, or <c>null</c>.</returns>
         string LoadExceptionDump();
+        
         /// <summary>
         /// Removes any exception dump from disc, if it exists.
         /// </summary>
