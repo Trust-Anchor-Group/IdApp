66048a06074fd599c66bf094dc6eeaa34064583b
Peter Waher <peterwaher@hotmail.com>
2021-10-05 15:37:04 -0300

    Adding behaviour

---
+++ IdApp/IdApp/Behaviors/ReRenderOnClickedBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace IdApp.Behaviors
+{
+    /// <summary>
+    /// Used for Re-rendering a layout, when a button has been clicked.
+    /// </summary>
+    public class ReRenderOnClickedBehavior : Behavior<Button>
+    {
+        /// <summary>
+        /// The layout to re-render.
+        /// </summary>
+        [TypeConverter(typeof(ReferenceTypeConverter))]
+        public Layout Layout { get; set; }
+
+        /// <inheritdoc/>
+        protected override void OnAttachedTo(Button Button)
+        {
+			Button.Clicked += Button_Clicked;
+            base.OnAttachedTo(Button);
+        }
+
+		/// <inheritdoc/>
+		protected override void OnDetachingFrom(Button Button)
+        {
+            Button.Clicked -= Button_Clicked;
+            base.OnDetachingFrom(Button);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            this.Layout?.ForceLayout();
+        }
+    }
+}
