44980774b4afcb9439008bf94188a7f0851d47c6
Peter Waher <peterwaher@hotmail.com>
2022-01-07 21:38:27 -0300

    Chat: Tap on entire control frame => focus on editor

---
+++ IdApp/IdApp/Behaviors/SetFocusOnTappedBehavior.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace IdApp.Behaviors
+{
+    /// <summary>
+    /// Used for moving focus to the next UI component when a view has been tapped.
+    /// </summary>
+    public class SetFocusOnTappedBehavior : Behavior<View>
+    {
+        /// <summary>
+        /// The view to move focus to.
+        /// </summary>
+        [TypeConverter(typeof(ReferenceTypeConverter))]
+        public View SetFocusTo { get; set; }
+
+        /// <inheritdoc/>
+        protected override void OnAttachedTo(View View)
+        {
+            TapGestureRecognizer Tap = new TapGestureRecognizer();
+            View.GestureRecognizers.Add(Tap);
+            Tap.Tapped += Tap_Tapped;
+
+            base.OnAttachedTo(View);
+        }
+
+		/// <inheritdoc/>
+		protected override void OnDetachingFrom(View View)
+        {
+            foreach (IGestureRecognizer Gesture in View.GestureRecognizers)
+			{
+                if (Gesture is TapGestureRecognizer Tap)
+                {
+                    Tap.Tapped -= Tap_Tapped;
+                    View.GestureRecognizers.Remove(Tap);
+                    break;
+                }
+			}
+
+            base.OnDetachingFrom(View);
+        }
+
+        private void Tap_Tapped(object sender, EventArgs e)
+        {
+            FocusOn(SetFocusTo);
+        }
+
+        /// <summary>
+        /// Sets focus on an element.
+        /// </summary>
+        /// <param name="Element">Element to focus on.</param>
+        public static void FocusOn(View Element)
+		{
+            if (!(Element is null) && Element.IsVisible)
+            {
+                Element.Focus();
+
+                if (Element is Entry Entry && !(Entry.Text is null))
+                    Entry.CursorPosition = Entry.Text.Length;
+            }
+        }
+    }
+}
