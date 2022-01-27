0473a6ed31228fa4e40d914c5dd05fdf729c4fc0
Peter Waher <peterwaher@hotmail.com>
2022-01-27 18:13:36 -0300

    Minor edit

--- IdApp/Version/Version.cs
+++ IdApp/Version/Version.cs
@@ -16,5 +16,5 @@ using System.Resources;
 [assembly: AssemblyTitle("TAG ID")]
 [assembly: AssemblyDescription("TAG ID App")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
