61bd0765cc4cd64525ebc3924a0caec05978a3cb
Ianos Gnatiuc <ignatiuc@netinfo.md>
2022-04-15 16:46:31 +0300

    Restrict the account name possible characters

--- IdApp/IdApp/Pages/Registration/ChooseAccount/ChooseAccountView.xaml.cs
+++ IdApp/IdApp/Pages/Registration/ChooseAccount/ChooseAccountView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace IdApp.Pages.Registration.ChooseAccount
 {
@@ -8,12 +10,41 @@ namespace IdApp.Pages.Registration.ChooseAccount
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ChooseAccountView
     {
+        private const int MaxAccountNameLength = 10;
+        private const string SpecialCharacters = "\x20\x22\x26\x27\x2F\x3A\x3C\x3E\x40\xA0" +
+            "\x1\x2\x3\x4\x5\x6\x7\x8\x9\xA\xB\xC\xD\xE\xF\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
+
         /// <summary>
         /// Creates a new instance of the <see cref="ChooseAccountView"/> class.
         /// </summary>
         public ChooseAccountView()
         {
             InitializeComponent();
+        }
+
+        private void AccountNameTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string NewTextValue = e.NewTextValue;
+
+            if (!string.IsNullOrWhiteSpace(NewTextValue))
+            {
+                if (NewTextValue.Length > MaxAccountNameLength)
+                {
+                    NewTextValue = NewTextValue.Remove(MaxAccountNameLength);
+                }
+
+                for (int i = 0; i < NewTextValue.Length; i++)
+                {
+                    char x = NewTextValue[i];
+
+                    if (SpecialCharacters.Contains(x))
+                    {
+                        NewTextValue = NewTextValue.Remove(i);
+                    }
+                }
+            }
+
+            ((Entry)sender).Text = NewTextValue;
         }
     }
 }
