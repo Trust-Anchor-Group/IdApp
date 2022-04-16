108a8d190c29c08f148ebf5cac1f8cb53a875a70
Ianos Gnatiuc <ignatiuc@netinfo.md>
2022-04-17 00:29:54 +0300

    fix

--- IdApp/IdApp/Pages/Registration/ChooseAccount/ChooseAccountView.xaml.cs
+++ IdApp/IdApp/Pages/Registration/ChooseAccount/ChooseAccountView.xaml.cs
@@ -10,7 +10,7 @@ namespace IdApp.Pages.Registration.ChooseAccount
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ChooseAccountView
     {
-        private const int MaxAccountNameLength = 10;
+        private const int MaxAccountNameLength = 64;
         private const string SpecialCharacters = "\x20\x22\x26\x27\x2F\x3A\x3C\x3E\x40\xA0" +
             "\x1\x2\x3\x4\x5\x6\x7\x8\x9\xA\xB\xC\xD\xE\xF\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
 
