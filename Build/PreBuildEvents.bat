810b16a1d4d71769bfee23cb830b1262f07718e6
Ianos Gnatiuc <ignatiuc@netinfo.md>
2022-04-19 17:34:13 +0300

    iOS pre build event

--- Build/PreBuildEvents.bat
+++ Build/PreBuildEvents.bat
@@ -1,1 +1,2 @@
 copy /Y "..\..\TagDemo Cert\google-services.json" "..\..\IdApp\IdApp.Android\"
+copy /Y "..\..\TagDemo Cert\GoogleService-Info.plist" "..\..\IdApp\IdApp.iOS\"
--- Build/PreBuildEvents.sh
+++ Build/PreBuildEvents.sh
@@ -1,1 +1,2 @@
 cp "../../TagDemo Cert/google-services.json" "../../IdApp/IdApp.Android/"
+cp "../../TagDemo Cert/GoogleService-Info.plist" "../../IdApp/IdApp.iOS/"
