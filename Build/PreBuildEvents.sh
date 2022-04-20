622a5c1204dd7c38042797986848f579cb138a70
Ianos Gnatiuc <ignatiuc@netinfo.md>
2022-04-20 13:15:59 +0300

    Remove the automatic pre build event. Run it manually when it's needed

--- Build/PreBuildEvents.sh
+++ Build/PreBuildEvents.sh
@@ -1,2 +1,2 @@
-cp "../../TagDemo Cert/google-services.json" "../../IdApp/IdApp.Android/"
-cp "../../TagDemo Cert/GoogleService-Info.plist" "../../IdApp/IdApp.iOS/"
+cp "../TagDemo Cert/google-services.json" "../IdApp/IdApp.Android/google-services.json"
+cp "../TagDemo Cert/GoogleService-Info.plist" "../IdApp/IdApp.iOS/GoogleService-Info.plist"
