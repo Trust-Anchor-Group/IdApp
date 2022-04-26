1dcd519ec4506503ff4aeb59dbe459b04798b1f4
Ianos Gnatiuc <ignatiuc@netinfo.md>
2022-04-26 14:50:30 +0300

    Hide pending amount if it's zero

---
+++ IdApp/IdApp/Converters/GreaterThanZero.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IdApp.Converters
+{
+    /// <summary>
+    /// Is true if a property is greater than zero.
+    /// </summary>
+    public class GreaterThanZero : IValueConverter, IMarkupExtension
+    {
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal v)
+            {
+                return v > 0;
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Feature not implemented.");
+        }
+
+        /// <inheritdoc/>
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
