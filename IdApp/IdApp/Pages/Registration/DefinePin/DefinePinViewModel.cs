3cb9fe55f0d04bc0d0a48e52d27094911a024b02
Anton Sakovich <anton.sakovich@softteco.com>
2022-06-01 19:42:33 +0300

    Update `DefinePinView` and its view model
    Add PIN strength validation, corresponding error messages, and password
    mask togglers.

--- IdApp/IdApp/Pages/Registration/DefinePin/DefinePinViewModel.cs
+++ IdApp/IdApp/Pages/Registration/DefinePin/DefinePinViewModel.cs
@@ -7,267 +7,249 @@ using Xamarin.Forms;
 using IdApp.Services.Xmpp;
 using IdApp.Services.Tag;
 using IdApp.Resx;
+using System.Runtime.CompilerServices;
 
 namespace IdApp.Pages.Registration.DefinePin
 {
-    /// <summary>
-    /// The view model to bind to when showing Step 5 of the registration flow: defining a PIN.
-    /// </summary>
-    public class DefinePinViewModel : RegistrationStepViewModel
-    {
-        /// <summary>
-        /// Creates a new instance of the <see cref="DefinePinViewModel"/> class.
-        /// </summary>
-        public DefinePinViewModel()
-            : base(RegistrationStep.Pin)
-        {
-            this.ContinueCommand = new Command(_ => Continue(), _ => CanContinue());
-            this.SkipCommand = new Command(_ => Skip(), _ => CanSkip());
-            this.Title = AppResources.DefinePin;
-            this.PinIsTooShortMessage = string.Format(AppResources.PinTooShort, Constants.Authentication.MinPinLength);
-        }
+	/// <summary>
+	/// The view model to bind to when showing Step 5 of the registration flow: defining a PIN.
+	/// </summary>
+	public class DefinePinViewModel : RegistrationStepViewModel
+	{
+		/// <summary>
+		/// Creates a new instance of the <see cref="DefinePinViewModel"/> class.
+		/// </summary>
+		public DefinePinViewModel()
+			: base(RegistrationStep.Pin)
+		{
+			this.ContinueCommand = new Command(_ => this.Continue(), _ => this.CanContinue());
+			this.SkipCommand = new Command(_ => this.Skip(), _ => this.CanSkip());
+			this.Title = AppResources.DefinePin;
+		}
 
-        /// <inheritdoc />
-        protected override async Task DoBind()
-        {
-            await base.DoBind();
-            AssignProperties(this.XmppService.State);
-            this.XmppService.ConnectionStateChanged += XmppService_ConnectionStateChanged;
-        }
+		/// <inheritdoc />
+		protected override async Task DoBind()
+		{
+			await base.DoBind();
+			this.AssignProperties(this.XmppService.State);
+			this.XmppService.ConnectionStateChanged += this.XmppService_ConnectionStateChanged;
+		}
 
-        /// <inheritdoc />
-        protected override async Task DoUnbind()
-        {
-            this.XmppService.ConnectionStateChanged -= XmppService_ConnectionStateChanged;
-            await base.DoUnbind();
-        }
+		/// <inheritdoc />
+		protected override async Task DoUnbind()
+		{
+			this.XmppService.ConnectionStateChanged -= this.XmppService_ConnectionStateChanged;
+			await base.DoUnbind();
+		}
 
-        /// <inheritdoc />
-        public override void ClearStepState()
-        {
-            this.Pin = string.Empty;
-            this.RetypedPin = string.Empty;
-        }
+		/// <inheritdoc />
+		public override void ClearStepState()
+		{
+			this.Pin = string.Empty;
+			this.RetypedPin = string.Empty;
+		}
 
-        #region Properties
+		#region Properties
 
-        /// <summary>
-        /// The command to bind to for continuing to the next step in the registration flow.
-        /// </summary>
-        public ICommand ContinueCommand { get; }
-        /// <summary>
-        /// The command to bind to for skipping this step and moving on to the next step in the registration flow.
-        /// </summary>
-        public ICommand SkipCommand { get; }
+		/// <summary>
+		/// The command to bind to for continuing to the next step in the registration flow.
+		/// </summary>
+		public ICommand ContinueCommand { get; }
+		/// <summary>
+		/// The command to bind to for skipping this step and moving on to the next step in the registration flow.
+		/// </summary>
+		public ICommand SkipCommand { get; }
 
-        /// <summary>
-        /// The <see cref="Pin"/>
-        /// </summary>
-        public static readonly BindableProperty PinProperty =
-            BindableProperty.Create(nameof(Pin), typeof(string), typeof(DefinePinViewModel), string.Empty, propertyChanged: (b, oldValue, newValue) =>
-            {
-                DefinePinViewModel viewModel = (DefinePinViewModel)b;
-                viewModel.UpdatePinState();
-                viewModel.ContinueCommand.ChangeCanExecute();
-            });
+		/// <summary>
+		/// The <see cref="Pin"/>
+		/// </summary>
+		public static readonly BindableProperty PinProperty =
+			BindableProperty.Create(nameof(Pin), typeof(string), typeof(DefinePinViewModel), string.Empty);
 
-        /// <summary>
-        /// The PIn code entered by the user.
-        /// </summary>
-        public string Pin
-        {
-            get => (string)this.GetValue(PinProperty);
-            set => this.SetValue(PinProperty, value);
-        }
+		/// <summary>
+		/// The PIn code entered by the user.
+		/// </summary>
+		public string Pin
+		{
+			get => (string)this.GetValue(PinProperty);
+			set => this.SetValue(PinProperty, value);
+		}
 
-        /// <summary>
-        /// The <see cref="RetypedPin"/>
-        /// </summary>
-        public static readonly BindableProperty RetypedPinProperty =
-            BindableProperty.Create(nameof(RetypedPin), typeof(string), typeof(DefinePinViewModel), default(string), propertyChanged: (b, oldValue, newValue) =>
-            {
-                DefinePinViewModel viewModel = (DefinePinViewModel)b;
-                viewModel.UpdatePinState();
-                viewModel.ContinueCommand.ChangeCanExecute();
-            });
+		/// <summary>
+		/// The <see cref="RetypedPin"/>
+		/// </summary>
+		public static readonly BindableProperty RetypedPinProperty =
+			BindableProperty.Create(nameof(RetypedPin), typeof(string), typeof(DefinePinViewModel), default(string));
 
-        /// <summary>
-        /// The retyped pin to use for validation against <see cref="Pin"/>.
-        /// </summary>
-        public string RetypedPin
-        {
-            get => (string)this.GetValue(RetypedPinProperty);
-            set => this.SetValue(RetypedPinProperty, value);
-        }
+		/// <summary>
+		/// The retyped pin to use for validation against <see cref="Pin"/>.
+		/// </summary>
+		public string RetypedPin
+		{
+			get => (string)this.GetValue(RetypedPinProperty);
+			set => this.SetValue(RetypedPinProperty, value);
+		}
 
-        private void UpdatePinState()
-        {
-            PinsDoNotMatch = !string.IsNullOrWhiteSpace(Pin) && !string.IsNullOrWhiteSpace(RetypedPin) && (Pin != RetypedPin);
-            PinIsTooShort = string.IsNullOrWhiteSpace(Pin) || Pin.Length < Constants.Authentication.MinPinLength;
-        }
+		/// <summary>
+		/// The <see cref="EnteringPinStarted"/>
+		/// </summary>
+		public static readonly BindableProperty EnteringPinStartedProperty =
+			BindableProperty.Create(nameof(EnteringPinStarted), typeof(bool), typeof(DefinePinViewModel), false);
 
-        /// <summary>
-        /// The <see cref="PinsDoNotMatch"/>
-        /// </summary>
-        public static readonly BindableProperty PinsDoNotMatchProperty =
-            BindableProperty.Create(nameof(PinsDoNotMatch), typeof(bool), typeof(DefinePinViewModel), default(bool));
+		/// <summary>
+		/// Gets or sets a value indicating if the user has started entering PIN.
+		/// </summary>
+		public bool EnteringPinStarted
+		{
+			get => (bool)this.GetValue(EnteringPinStartedProperty);
+			set => this.SetValue(EnteringPinStartedProperty, value);
+		}
 
-        /// <summary>
-        /// Gets or sets whether the entered Pin differ betweeen <see cref="Pin"/> and <see cref="RetypedPin"/>.
-        /// </summary>
-        public bool PinsDoNotMatch
-        {
-            get => (bool)this.GetValue(PinsDoNotMatchProperty);
-            set => this.SetValue(PinsDoNotMatchProperty, value);
-        }
+		/// <summary>
+		/// Gets the value indicating how strong the <see cref="Pin"/> is.
+		/// </summary>
+		public PinStrength PinStrength => this.TagProfile.ValidatePinStrength(this.Pin);
 
-        /// <summary>
-        /// The <see cref="PinIsTooShort"/>
-        /// </summary>
-        public static readonly BindableProperty PinIsTooShortProperty =
-            BindableProperty.Create(nameof(PinIsTooShort), typeof(bool), typeof(DefinePinViewModel), default(bool));
+		/// <summary>
+		/// Gets a value indicating whether the entered PIN differs between <see cref="Pin"/> and <see cref="RetypedPin"/>.
+		/// </summary>
+		public bool PinsDoNotMatch => !string.IsNullOrWhiteSpace(this.Pin) && !string.IsNullOrWhiteSpace(this.RetypedPin) && (this.Pin != this.RetypedPin);
 
-        /// <summary>
-        /// Gets or sets whether the entered PIN is too short.
-        /// </summary>
-        public bool PinIsTooShort
-        {
-            get => (bool)this.GetValue(PinIsTooShortProperty);
-            set => this.SetValue(PinIsTooShortProperty, value);
-        }
+		/// <summary>
+		/// The <see cref="UsePin"/>
+		/// </summary>
+		public static readonly BindableProperty UsePinProperty =
+			BindableProperty.Create(nameof(UsePin), typeof(bool), typeof(DefinePinViewModel), default(bool));
 
-        /// <summary>
-        /// The <see cref="PinIsTooShortMessage"/>
-        /// </summary>
-        public static readonly BindableProperty PinIsTooShortMessageProperty =
-            BindableProperty.Create(nameof(PinIsTooShortMessage), typeof(string), typeof(DefinePinViewModel), default(string));
+		/// <summary>
+		/// Gets or sets whether a PIn should be used for validation.
+		/// </summary>
+		public bool UsePin
+		{
+			get => (bool)this.GetValue(UsePinProperty);
+			set => this.SetValue(UsePinProperty, value);
+		}
 
-        /// <summary>
-        /// The localized message to display if and when the PIN code is too short.
-        /// </summary>
-        public string PinIsTooShortMessage
-        {
-            get => (string)this.GetValue(PinIsTooShortMessageProperty);
-            set => this.SetValue(PinIsTooShortMessageProperty, value);
-        }
+		/// <summary>
+		/// The <see cref="IsConnected"/>
+		/// </summary>
+		public static readonly BindableProperty IsConnectedProperty =
+			BindableProperty.Create(nameof(IsConnected), typeof(bool), typeof(DefinePinViewModel), default(bool));
 
-        /// <summary>
-        /// The <see cref="UsePin"/>
-        /// </summary>
-        public static readonly BindableProperty UsePinProperty =
-            BindableProperty.Create(nameof(UsePin), typeof(bool), typeof(DefinePinViewModel), default(bool));
+		/// <summary>
+		/// Gets or sets if the app is connected to an XMPP server.
+		/// </summary>
+		public bool IsConnected
+		{
+			get => (bool)this.GetValue(IsConnectedProperty);
+			set => this.SetValue(IsConnectedProperty, value);
+		}
 
-        /// <summary>
-        /// Gets or sets whether a PIn should be used for validation.
-        /// </summary>
-        public bool UsePin
-        {
-            get => (bool)this.GetValue(UsePinProperty);
-            set => this.SetValue(UsePinProperty, value);
-        }
+		/// <summary>
+		/// The <see cref="ConnectionStateText"/>
+		/// </summary>
+		public static readonly BindableProperty ConnectionStateTextProperty =
+			BindableProperty.Create(nameof(ConnectionStateText), typeof(string), typeof(DefinePinViewModel), default(string));
 
-        /// <summary>
-        /// The <see cref="IsConnected"/>
-        /// </summary>
-        public static readonly BindableProperty IsConnectedProperty =
-            BindableProperty.Create(nameof(IsConnected), typeof(bool), typeof(DefinePinViewModel), default(bool));
+		/// <summary>
+		/// The user friendly connection state text to display to the user.
+		/// </summary>
+		public string ConnectionStateText
+		{
+			get => (string)this.GetValue(ConnectionStateTextProperty);
+			set => this.SetValue(ConnectionStateTextProperty, value);
+		}
 
-        /// <summary>
-        /// Gets or sets if the app is connected to an XMPP server.
-        /// </summary>
-        public bool IsConnected
-        {
-            get => (bool)this.GetValue(IsConnectedProperty);
-            set => this.SetValue(IsConnectedProperty, value);
-        }
+		#endregion
 
-        /// <summary>
-        /// The <see cref="ConnectionStateText"/>
-        /// </summary>
-        public static readonly BindableProperty ConnectionStateTextProperty =
-            BindableProperty.Create(nameof(ConnectionStateText), typeof(string), typeof(DefinePinViewModel), default(string));
+		/// <inheritdoc/>
+		protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			base.OnPropertyChanged(propertyName);
 
-        /// <summary>
-        /// The user friendly connection state text to display to the user.
-        /// </summary>
-        public string ConnectionStateText
-        {
-            get => (string)this.GetValue(ConnectionStateTextProperty);
-            set => this.SetValue(ConnectionStateTextProperty, value);
-        }
+			if (propertyName == nameof(this.Pin))
+			{
+				this.EnteringPinStarted = true;
+				this.OnPropertyChanged(nameof(this.PinStrength));
+			}
 
-        #endregion
+			if (propertyName == nameof(this.Pin) || propertyName == nameof(this.RetypedPin))
+			{
+				this.OnPropertyChanged(nameof(this.PinsDoNotMatch));
+				this.ContinueCommand.ChangeCanExecute();
+			}
+		}
 
-        private void XmppService_ConnectionStateChanged(object sender, ConnectionStateChangedEventArgs e)
-        {
-            this.UiSerializer.BeginInvokeOnMainThread(() =>
-            {
-                AssignProperties(e.State);
-            });
-        }
+		private void XmppService_ConnectionStateChanged(object sender, ConnectionStateChangedEventArgs e)
+		{
+			this.UiSerializer.BeginInvokeOnMainThread(() =>
+			{
+				this.AssignProperties(e.State);
+			});
+		}
 
-        private void AssignProperties(XmppState state)
-        {
-            SetConnectionStateAndText(state);
-            ContinueCommand.ChangeCanExecute();
-            SkipCommand.ChangeCanExecute();
-        }
+		private void AssignProperties(XmppState state)
+		{
+			this.SetConnectionStateAndText(state);
+			this.ContinueCommand.ChangeCanExecute();
+			this.SkipCommand.ChangeCanExecute();
+		}
 
-        private void SetConnectionStateAndText(XmppState state)
-        {
-            IsConnected = state == XmppState.Connected;
-            this.ConnectionStateText = state.ToDisplayText();
-        }
+		private void SetConnectionStateAndText(XmppState state)
+		{
+			this.IsConnected = state == XmppState.Connected;
+			this.ConnectionStateText = state.ToDisplayText();
+		}
 
-        private void Skip()
-        {
-            UsePin = false;
+		private void Skip()
+		{
+			this.UsePin = false;
 
-            Complete();
-        }
+			this.Complete();
+		}
 
-        private bool CanSkip()
-        {
-            return this.XmppService.IsOnline;
-        }
+		private bool CanSkip()
+		{
+			return this.XmppService.IsOnline;
+		}
 
-        private void Continue()
-        {
-            UsePin = true;
+		private void Continue()
+		{
+			this.UsePin = true;
 
-            string pinToCheck = this.Pin ?? string.Empty;
+			string pinToCheck = this.Pin ?? string.Empty;
 
-            if (pinToCheck.Length < Constants.Authentication.MinPinLength)
-            {
-                this.UiSerializer.DisplayAlert(AppResources.ErrorTitle, string.Format(AppResources.PinTooShort, Constants.Authentication.MinPinLength));
-                return;
-            }
-            if (pinToCheck.Trim() != pinToCheck)
-            {
-                this.UiSerializer.DisplayAlert(AppResources.ErrorTitle, AppResources.PinMustNotIncludeWhitespace);
-                return;
-            }
+			if (pinToCheck.Length < Constants.Authentication.MinPinLength)
+			{
+				this.UiSerializer.DisplayAlert(AppResources.ErrorTitle, string.Format(AppResources.PinTooShort, Constants.Authentication.MinPinLength));
+				return;
+			}
+			if (pinToCheck.Trim() != pinToCheck)
+			{
+				this.UiSerializer.DisplayAlert(AppResources.ErrorTitle, AppResources.PinMustNotIncludeWhitespace);
+				return;
+			}
 
-            Complete();
-        }
+			this.Complete();
+		}
 
 
-        private void Complete()
-        {
+		private void Complete()
+		{
 
-            this.TagProfile.SetPin(this.Pin, this.UsePin);
+			this.TagProfile.SetPin(this.Pin, this.UsePin);
 
-            OnStepCompleted(EventArgs.Empty);
+			this.OnStepCompleted(EventArgs.Empty);
 
-            if (this.TagProfile.TestOtpTimestamp is not null)
-            {
-                this.UiSerializer.DisplayAlert(AppResources.WarningTitle, AppResources.TestOtpUsed, AppResources.Ok);
-            }
-        }
+			if (this.TagProfile.TestOtpTimestamp is not null)
+			{
+				this.UiSerializer.DisplayAlert(AppResources.WarningTitle, AppResources.TestOtpUsed, AppResources.Ok);
+			}
+		}
 
-        private bool CanContinue()
-        {
-            return !PinsDoNotMatch && this.XmppService.IsOnline;
-        }
-    }
+		private bool CanContinue()
+		{
+			return this.PinStrength == PinStrength.Strong && !this.PinsDoNotMatch && this.XmppService.IsOnline;
+		}
+	}
 }
