374174cd027f14b5a4ae4d25c20b4a515c8ff38a
Anton Sakovich <anton.sakovich@softteco.com>
2022-06-13 19:58:41 +0300

    Update `CustomEntryRenderer`
    Add support for a border around the `Entry`.

--- IdApp/IdApp.Android/Renderers/CustomEntryRenderer.cs
+++ IdApp/IdApp.Android/Renderers/CustomEntryRenderer.cs
@@ -1,13 +1,23 @@
-﻿using Android.Content;
+﻿using System.ComponentModel;
+using System.Linq;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Graphics.Drawables.Shapes;
 using IdApp.Android.Renderers;
+using IdApp.Helpers;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
+using AndroidColor = Android.Graphics.Color;
 
 [assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer), new[] { typeof(VisualMarker.DefaultVisual) })]
 namespace IdApp.Android.Renderers
 {
 	public class CustomEntryRenderer : EntryRenderer
 	{
+		private const double cornerRadiusInDip = 5;
+		private const double borderWidthInDip = 1;
+
 		public CustomEntryRenderer(Context context) : base(context) { }
 
 		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
@@ -16,8 +26,24 @@ namespace IdApp.Android.Renderers
 
 			if (this.Control is not null)
 			{
-				this.Control.Background = null;
-				this.Control.SetBackgroundColor(global::Android.Graphics.Color.Transparent);
+				float CornerRadius = this.Context.ToPixels(cornerRadiusInDip);
+				ShapeDrawable Background = new(new RoundRectShape(Enumerable.Repeat(CornerRadius, 8).ToArray(), null, null));
+
+				Background.Paint.SetStyle(Paint.Style.Stroke);
+				Background.Paint.StrokeWidth = this.Context.ToPixels(borderWidthInDip);
+				Background.Paint.Color = this.Element is Entry Entry ? EntryProperties.GetBorderColor(Entry).ToAndroid() : AndroidColor.Transparent;
+
+				this.Control.Background = Background;
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object Sender, PropertyChangedEventArgs EventArgs)
+		{
+			base.OnElementPropertyChanged(Sender, EventArgs);
+
+			if (EventArgs.PropertyName == EntryProperties.BorderColorProperty.PropertyName && this.Control?.Background is ShapeDrawable ShapeDrawable)
+			{
+				ShapeDrawable.Paint.Color = EntryProperties.GetBorderColor(this.Element).ToAndroid();
 			}
 		}
 	}
