7aa34a44e726f517add7ad61730eb8544dc682b4
Anton Sakovich <anton.sakovich@softteco.com>
2022-06-13 19:54:45 +0300

    Add `EntryProperties` class
    `EntryProperties` is a class which defines attached properties used in
    our custom renderer for `Entry`.

---
+++ IdApp/IdApp/Helpers/EntryProperties.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace IdApp.Helpers
+{
+	public static class EntryProperties
+	{
+		public static readonly BindableProperty BorderColorProperty = BindableProperty.CreateAttached("BorderColor", typeof(Color), typeof(EntryProperties),
+			defaultValue: Color.Transparent);
+
+		public static Color GetBorderColor(BindableObject Bindable)
+		{
+			return (Color)Bindable.GetValue(BorderColorProperty);
+		}
+
+		public static void SetBorderColor(BindableObject Bindable, Color Color)
+		{
+			Bindable.SetValue(BorderColorProperty, Color);
+		}
+	}
+}
