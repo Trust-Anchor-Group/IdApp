eab80a02aafe7335c61fab44e51e9833c10e8fdb
Anton Sakovich <anton.sakovich@softteco.com>
2022-07-01 12:57:57 +0300

    Add `IdApp.Resx.Pngs` static helper class

---
+++ IdApp/IdApp/Resx/Pngs.cs
@@ -0,0 +1,18 @@
+﻿namespace IdApp.Resx
+{
+	/// <summary>
+	/// A list of png resources.
+	/// </summary>
+	public static class Pngs
+	{
+		/// <summary>
+		/// An image icon from Google Fonts.
+		/// </summary>
+		public const string Image = "IdApp.Resx.Pngs.image.png";
+
+		/// <summary>
+		/// A broken image icon from Google Fonts.
+		/// </summary>
+		public const string BrokenImage = "IdApp.Resx.Pngs.broken_image.png";
+	}
+}
