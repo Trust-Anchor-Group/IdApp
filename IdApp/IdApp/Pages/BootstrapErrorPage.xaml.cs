58a49a3fb57416d81640ea0ec050f55285ecbcb4
Anton Sakovich <anton.sakovich@softteco.com>
2022-07-04 20:06:02 +0300

    Add `BootstrapErrorPage`

---
+++ IdApp/IdApp/Pages/BootstrapErrorPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IdApp.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BootstrapErrorPage : ContentBasePage
+	{
+		public BootstrapErrorPage()
+		{
+			InitializeComponent();
+		}
+
+		protected override bool OnBackButtonPressed()
+		{
+			return true;
+		}
+	}
+}
