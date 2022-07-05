06e4933da27043ef80e4f66c94ceaf40ffd42b11
Anton Sakovich <anton.sakovich@softteco.com>
2022-07-05 13:27:08 +0300

    Update `BootstrapErrorPage.xaml.cs`
    Remove unnecessary usings, add XML summaries, etc.
    Return `false` from `OnBackButtonPressed`.

--- IdApp/IdApp/Pages/BootstrapErrorPage.xaml.cs
+++ IdApp/IdApp/Pages/BootstrapErrorPage.xaml.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms.Xaml;
 
 namespace IdApp.Pages
 {
+	/// <summary>
+	/// A page which is displayed when an unexpected exception is encountered during the application startup.
+	/// </summary>
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class BootstrapErrorPage : ContentBasePage
 	{
+		/// <summary>
+		/// Creates a new instance of the <see cref="BootstrapErrorPage"/> class.
+		/// </summary>
 		public BootstrapErrorPage()
 		{
-			InitializeComponent();
+			this.InitializeComponent();
 		}
 
+		/// <inheritdoc/>
 		protected override bool OnBackButtonPressed()
 		{
-			return true;
+			return false;
 		}
 	}
 }
