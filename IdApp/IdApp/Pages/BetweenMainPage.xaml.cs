97adfabf29b9c4b6638234b16ccb522f9aa0ee09
Anton Sakovich <anton.sakovich@softteco.com>
2022-07-21 18:40:30 +0300

    Add `BetweenMainPage`
    `BetweenMainPage` is a page which is displayed when switching
    the application main page, while the old main page is performing
    cleanup asynchronously.

---
+++ IdApp/IdApp/Pages/BetweenMainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace IdApp.Pages
+{
+	/// <summary>
+	/// A page which is displayed when switching the application main page, while the old main page is performing cleanup asynchronously.
+	/// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BetweenMainPage : ContentBasePage
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BetweenMainPage"/> class.
+		/// </summary>
+		public BetweenMainPage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
