fd219df1345d701f893b397580c15d8b44256b50
Anton Sakovich <anton.sakovich@softteco.com>
2022-07-22 13:22:35 +0300

    Add `IdAppNavigationRenderer`
    A custom renderer for navigation pages on iOS is used to suppress
    messing up with status bar style which happens inside the default
    Xamarin Forms renderer.
    
    https://github.com/xamarin/Xamarin.Forms/issues/4343

---
+++ IdApp/IdApp.iOS/Renderers/IdAppNavigationRenderer.cs
@@ -0,0 +1,18 @@
+﻿using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(NavigationPage), typeof(IdApp.iOS.Renderers.IdAppNavigationRenderer))]
+namespace IdApp.iOS.Renderers
+{
+	public class IdAppNavigationRenderer : NavigationRenderer
+	{
+		public override void ViewWillAppear(bool animated)
+		{
+			// https://github.com/xamarin/Xamarin.Forms/issues/4343
+			UIStatusBarStyle StatusBarStyle = UIApplication.SharedApplication.StatusBarStyle;
+			base.ViewWillAppear(animated);
+			UIApplication.SharedApplication.StatusBarStyle = StatusBarStyle;
+		}
+	}
+}
