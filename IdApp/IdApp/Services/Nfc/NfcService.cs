df091b3c9a44cee4b381cc42711d38b4baf9f2a5
Peter Waher <peterwaher@hotmail.com>
2022-07-22 14:48:05 -0400

    Reading NFCs

--- IdApp/IdApp/Services/Nfc/NfcService.cs
+++ IdApp/IdApp/Services/Nfc/NfcService.cs
@@ -1,5 +1,6 @@
 ﻿using IdApp.Nfc;
 using IdApp.Nfc.Extensions;
+using IdApp.Nfc.Records;
 using System;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
@@ -36,8 +37,10 @@ namespace IdApp.Services.Nfc
 
 			foreach (INfcInterface Interface in Tag.Interfaces)
 			{
-				if (Interface is IIsoDepInterface Iso14443_4)
+				if (Interface is IIsoDepInterface IsoDep)
 				{
+					// ISO 14443-4
+
 					string Mrz = await RuntimeSettings.GetAsync("NFC.LastMrz", string.Empty);
 
 					if (!string.IsNullOrEmpty(Mrz) &&
@@ -45,15 +48,74 @@ namespace IdApp.Services.Nfc
 					{
 						// §4.3, §D.3, https://www.icao.int/publications/Documents/9303_p11_cons_en.pdf
 
-						byte[] Challenge = await Iso14443_4.GetChallenge();
+						byte[] Challenge = await IsoDep.GetChallenge();
 						if (!(Challenge is null))
 						{
 							byte[] ChallengeResponse = DocInfo.CalcChallengeResponse(Challenge);
-							byte[] Response = await Iso14443_4.ExternalAuthenticate(ChallengeResponse);
+							byte[] Response = await IsoDep.ExternalAuthenticate(ChallengeResponse);
 
+							// TODO
 						}
 					}
 				}
+				else if (Interface is INdefInterface Ndef)
+				{
+					bool CanMakeReadOnly = await Ndef.CanMakeReadOnly();
+					bool IsWritable = await Ndef.IsWritable();
+					INdefRecord[] Records = await Ndef.GetMessage();
+
+					// TODO
+				}
+				//else if (Interface is INfcAInterface NfcA)
+				//{
+				//	byte[] Atqa = await NfcA.GetAtqa();
+				//	short Sqk = await NfcA.GetSqk();
+				//
+				//	// TODO
+				//}
+				//else if (Interface is INfcBInterface NfcB)
+				//{
+				//	byte[] ApplicationData = await NfcB.GetApplicationData();
+				//	byte[] ProtocolInfo = await NfcB.GetProtocolInfo();
+				//
+				//	// TODO
+				//}
+				//else if (Interface is INfcFInterface NfcF)
+				//{
+				//	byte[] Manufacturer = await NfcF.GetManufacturer();
+				//	byte[] SystemCode = await NfcF.GetSystemCode();
+				//
+				//	// TODO
+				//}
+				//else if (Interface is INfcVInterface NfcV)
+				//{
+				//	sbyte DsfId = await NfcV.GetDsfId();
+				//	short ResponseFlags = await NfcV.GetResponseFlags();
+				//
+				//	// TODO
+				//}
+				//else if (Interface is INfcBarcodeInterface Barcode)
+				//{
+				//	byte[] Data = await Barcode.ReadAllData();
+				//
+				//	// TODO
+				//}
+				//else if (Interface is INdefFormatableInterface NdefFormatable)
+				//{
+				//	// TODO
+				//}
+				//else if (Interface is IMifareUltralightInterface MifareUltralight)
+				//{
+				//	byte[] Data = await MifareUltralight.ReadAllData();
+				//
+				//	// TODO
+				//}
+				//else if (Interface is IMifareClassicInterface MifareClassic)
+				//{
+				//	byte[] Data = await MifareClassic.ReadAllData();
+				//
+				//	// TODO
+				//}
 			}
 		}
 	}
