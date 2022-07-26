2f21eaf1abff67f019590132ca925bb134c8b30a
Anton Sakovich <anton.sakovich@softteco.com>
2022-07-26 16:42:00 +0300

    Add a custom Shell renderer for iOS
    The custom renderer manually highlights the first shell item in order
    to work around https://github.com/xamarin/Xamarin.Forms/issues/14935.

---
+++ IdApp/IdApp.iOS/Renderers/IdAppShellFlyoutContentRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace IdApp.iOS.Renderers
+{
+	internal class IdAppShellFlyoutContentRenderer : ShellFlyoutContentRenderer
+	{
+		public ShellTableViewController TableViewController;
+
+		public IdAppShellFlyoutContentRenderer(IShellContext ShellContext) : base(ShellContext)
+		{
+		}
+
+		protected override ShellTableViewController CreateShellTableViewController()
+		{
+			this.TableViewController = base.CreateShellTableViewController();
+			return this.TableViewController;
+		}
+	}
+}
