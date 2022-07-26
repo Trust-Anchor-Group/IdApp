74229c40b03aa49b105c2ebeb6c23665fb5369c9
Anton Sakovich <anton.sakovich@softteco.com>
2022-07-26 20:28:54 +0300

    Update Shell-related renderers
    Select not just the first cell but the one which corresponds to the
    Shell's `CurrentItem`.
    https://github.com/xamarin/Xamarin.Forms/issues/14935

--- IdApp/IdApp.iOS/Renderers/IdAppShellFlyoutContentRenderer.cs
+++ IdApp/IdApp.iOS/Renderers/IdAppShellFlyoutContentRenderer.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-
 using Foundation;
 using UIKit;
 using Xamarin.Forms;
@@ -12,16 +10,66 @@ namespace IdApp.iOS.Renderers
 {
 	internal class IdAppShellFlyoutContentRenderer : ShellFlyoutContentRenderer
 	{
-		public ShellTableViewController TableViewController;
+		private readonly UITableView flyoutTable;
 
 		public IdAppShellFlyoutContentRenderer(IShellContext ShellContext) : base(ShellContext)
 		{
+			if (this.ChildViewControllers.OfType<ShellTableViewController>().FirstOrDefault() is ShellTableViewController Controller)
+			{
+				this.flyoutTable = Controller.TableView;
+				ShellContext.Shell.PropertyChanged += this.OnShellPropertyChanged;
+			}
 		}
 
-		protected override ShellTableViewController CreateShellTableViewController()
+		private void OnShellPropertyChanged(object Sender, PropertyChangedEventArgs Args)
 		{
-			this.TableViewController = base.CreateShellTableViewController();
-			return this.TableViewController;
+			Shell Shell = (Shell)Sender;
+			if (Args.PropertyName == nameof(Shell.FlyoutIsPresented) && Shell.FlyoutIsPresented)
+			{
+				Shell.PropertyChanged -= this.OnShellPropertyChanged;
+
+				// This is a work around for https://github.com/xamarin/Xamarin.Forms/issues/14935.
+				if (Shell.CurrentItem is ShellItem CurrentItem && this.flyoutTable.IndexPathForSelectedRow is null)
+				{
+					List<List<Element>> ShellGroupings = ((IShellController)Shell).GenerateFlyoutGrouping();
+
+					if (TryFindShellItemPath(CurrentItem, ShellGroupings) is NSIndexPath CurrentItemPath)
+					{
+						this.flyoutTable.SelectRow(CurrentItemPath, animated: false, scrollPosition: UITableViewScrollPosition.None);
+
+						if (this.flyoutTable.CellAt(CurrentItemPath) is UIContainerCell CurrentItemCell && CurrentItemCell.View is View CurrentItemView)
+						{
+							VisualStateManager.GoToState(CurrentItemView, "Selected");
+						}
+					}
+				}
+			}
+		}
+
+		private static NSIndexPath TryFindShellItemPath(ShellItem ShellItem, List<List<Element>> ShellGroupings)
+		{
+			bool Found = false;
+			int Section;
+			int Row = 0;
+
+			for (Section = 0; Section < ShellGroupings.Count; Section++)
+			{
+				for (Row = 0; Row < ShellGroupings[Section].Count; Row++)
+				{
+					if (ShellGroupings[Section][Row] == ShellItem)
+					{
+						Found = true;
+						break;
+					}
+				}
+
+				if (Found)
+				{
+					break;
+				}
+			}
+
+			return Found ? NSIndexPath.Create(Section, Row) : null;
 		}
 	}
 }
--- IdApp/IdApp.iOS/Renderers/IdAppShellRenderer.cs
+++ IdApp/IdApp.iOS/Renderers/IdAppShellRenderer.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-
-using Foundation;
-using UIKit;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
 [assembly: ExportRenderer(typeof(Shell), typeof(IdApp.iOS.Renderers.IdAppShellRenderer))]
@@ -14,36 +6,9 @@ namespace IdApp.iOS.Renderers
 {
 	public class IdAppShellRenderer : ShellRenderer
 	{
-		private IdAppShellFlyoutContentRenderer flyoutContentRenderer;
-
 		protected override IShellFlyoutContentRenderer CreateShellFlyoutContentRenderer()
 		{
-			return this.flyoutContentRenderer = new IdAppShellFlyoutContentRenderer(this);
-		}
-
-		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
-		{
-			base.OnElementPropertyChanged(sender, e);
-
-			if (e.PropertyName == Shell.FlyoutIsPresentedProperty.PropertyName && ((Shell)sender).FlyoutIsPresented)
-			{
-				UITableView TableView = this.flyoutContentRenderer?.TableViewController.TableView;
-				if (TableView != null)
-				{
-					NSIndexPath SelectedRowIndex = TableView?.IndexPathForSelectedRow;
-					if (SelectedRowIndex == null)
-					{
-						TableView.SelectRow(NSIndexPath.Create(0, 0), animated: false, scrollPosition: UITableViewScrollPosition.None);
-						SelectedRowIndex = TableView.IndexPathForSelectedRow;
-					}
-
-					UIContainerCell SelectedCell = (UIContainerCell)TableView.CellAt(SelectedRowIndex);
-					if (SelectedCell.View is View CellView)
-					{
-						VisualStateManager.GoToState(CellView, "Selected");
-					}
-				}
-			}
+			return new IdAppShellFlyoutContentRenderer(this);
 		}
 	}
 }
