fbf86003d6453db7c824eb2c7f8e91606d0d9f90
Anton Sakovich <anton.sakovich@softteco.com>
2022-07-27 20:39:47 +0300

    Update `PinStrengthTests`
    Return back `using IdApp.Services;`.
    
    We reverted moving `TagConfiguration` from `IdApp.Services` namespace
    to `IdApp.Services.Tag` namespace but forgot to return back the using
    directive. See
    47490d76affd717339912baa80d53b1d5a0d74f5
    and
    6684e3807badeeca8449cc0293f321c6596c1b05.

--- IdApp.Test/Security/PinStrengthTests.cs
+++ IdApp.Test/Security/PinStrengthTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using IdApp.Services;
 using IdApp.Services.Tag;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Waher.Networking.XMPP.Contracts;
