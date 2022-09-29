ac80c1442407987151fe9a9687b44338b911c128
Ianos Gnatiuc <ignatiuc@netinfo.md>
2022-09-29 16:39:03 +0300

    the theming

--- IdApp/IdApp/App.xaml.cs
+++ IdApp/IdApp/App.xaml.cs
@@ -146,11 +146,7 @@ namespace IdApp
 			}
 
 			this.InitializeComponent();
-#if DEBUG
 			Current.UserAppTheme = OSAppTheme.Unspecified;
-#else
-			Current.UserAppTheme = OSAppTheme.Light;
-#endif
 
 			// Start page
 			try
