02c109f8742c2704cc4526b5a135e487f4ad5aec
Ianos Gnatiuc <ignatiuc@netinfo.md>
2023-01-03 12:28:09 +0200

    version

--- Build/UpdateVersion.bat
+++ Build/UpdateVersion.bat
@@ -1,1 +1,1 @@
-UpdateVersionInfo.exe -v=1 -m=5 -b=44 -p=..\IdApp\Version\Version.cs -a=..\IdApp\IdApp.Android\Properties\AndroidManifest.xml -t=..\IdApp\IdApp.iOS\Info.plist 
+UpdateVersionInfo.exe -v=1 -m=5 -b=45 -p=..\IdApp\Version\Version.cs -a=..\IdApp\IdApp.Android\Properties\AndroidManifest.xml -t=..\IdApp\IdApp.iOS\Info.plist 
