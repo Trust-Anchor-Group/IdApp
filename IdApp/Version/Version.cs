70207a3fbd0b73c4868a7b1105854c4df099f0db
Ianos Gnatiuc <ignatiuc@netinfo.md>
2023-05-08 19:35:30 +0300

    version

--- IdApp/Version/Version.cs
+++ IdApp/Version/Version.cs
@@ -16,5 +16,5 @@ using System.Resources;
 [assembly: AssemblyTitle("TAG ID")]
 [assembly: AssemblyDescription("TAG ID App")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: System.Reflection.AssemblyVersion("1.8.63.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.8.63.0")]
+[assembly: System.Reflection.AssemblyVersion("1.8.64.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.8.64.0")]
