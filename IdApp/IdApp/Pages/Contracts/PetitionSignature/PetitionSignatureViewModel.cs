c1c991e02554e5fdae53a8db9ca93fb09a96c2c3
Peter Waher <peterwaher@hotmail.com>
2023-05-13 20:03:31 -0400

    Fix

--- IdApp/IdApp/Pages/Contracts/PetitionSignature/PetitionSignatureViewModel.cs
+++ IdApp/IdApp/Pages/Contracts/PetitionSignature/PetitionSignatureViewModel.cs
@@ -585,7 +585,26 @@ namespace IdApp.Pages.Contracts.PetitionSignature
 		public bool HasOrg
 		{
 			get => (bool)this.GetValue(HasOrgProperty);
-			set => this.SetValue(HasOrgProperty, value);
+			set
+			{
+				this.SetValue(HasOrgProperty, value);
+				this.OrgRowHeight = value ? GridLength.Auto : new GridLength(0, GridUnitType.Absolute);
+			}
+		}
+
+		/// <summary>
+		/// See <see cref="OrgRowHeight"/>
+		/// </summary>
+		public static readonly BindableProperty OrgRowHeightProperty =
+			BindableProperty.Create(nameof(OrgRowHeight), typeof(GridLength), typeof(PetitionSignatureViewModel), default(GridLength));
+
+		/// <summary>
+		/// If organization information is available.
+		/// </summary>
+		public GridLength OrgRowHeight
+		{
+			get => (GridLength)this.GetValue(OrgRowHeightProperty);
+			set => this.SetValue(OrgRowHeightProperty, value);
 		}
 
 		/// <summary>
