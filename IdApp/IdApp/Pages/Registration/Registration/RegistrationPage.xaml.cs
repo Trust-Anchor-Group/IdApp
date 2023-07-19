45a59fd7286a8a12ca6a3563675d66a7385fec4d
Ianos Gnatiuc <ignatiuc@netinfo.md>
2023-07-19 23:37:21 +0300

    Registration page rework part 1

--- IdApp/IdApp/Pages/Registration/Registration/RegistrationPage.xaml.cs
+++ IdApp/IdApp/Pages/Registration/Registration/RegistrationPage.xaml.cs
@@ -17,7 +17,6 @@ namespace IdApp.Pages.Registration.Registration
 
 		// See OnDisappearingAsync for details why we need these.
 		private View currentView;
-		private Rectangle currentViewBoundsBeforeDisappearing;
 
 		/// <summary>
 		/// Creates a new instance of the <see cref="RegistrationPage"/> class.
@@ -52,41 +51,8 @@ namespace IdApp.Pages.Registration.Registration
             {
                 await Task.Delay(TimeSpan.FromMilliseconds(100));
 				this.UpdateUiStep();
-
-				// this.UpdateUiStep(); uses BeginInvokeOnMainThread, so does this code in order to ensure that VisibleViews collection has been updated.
-				this.uiSerializer.BeginInvokeOnMainThread(() =>
-				{
-					// See OnDisappearingAsync as to why this is done.
-					if (this.currentView is not null && this.CarouselView.VisibleViews.Count == 1 && this.CarouselView.VisibleViews[0] == this.currentView)
-					{
-						this.CarouselView.VisibleViews[0].Layout(this.currentViewBoundsBeforeDisappearing);
-
-						this.currentView = null;
-						this.currentViewBoundsBeforeDisappearing = default;
-					}
-				});
             }
         }
-
-		/// <inheritdoc />
-		protected override Task OnDisappearingAsync()
-		{
-			// On Android, when we navigate from the registration page to a scan QR code page and return back, the size of the current view
-			// inside the carousel is incorrect, it is erroneously less than it was before the navigation (the size of the carousel itself).
-			// So, we memorize it now in order to restore it later.
-			if (this.CarouselView.VisibleViews.Count == 1)
-			{
-				this.currentView = this.CarouselView.VisibleViews[0];
-				this.currentViewBoundsBeforeDisappearing = this.currentView.Bounds;
-			}
-			else
-			{
-				this.currentView = null;
-				this.currentViewBoundsBeforeDisappearing = default;
-			}
-
-			return base.OnDisappearingAsync();
-		}
 
 		/// This is a hack. The issue is that the Carousel view doesn't reflect the CurrentStep binding correctly in the UI.
 		/// The viewmodel is correct. The position property on the CarouselView is correct. But during restarts
@@ -105,8 +71,8 @@ namespace IdApp.Pages.Registration.Registration
 						int OtherStep = Step + ((Step > 0) ? -1 : 1);
 
 						RegistrationViewModel.MuteStepSync();
-						this.CarouselView.ScrollTo(OtherStep, position: ScrollToPosition.Center, animate: false);
-						this.CarouselView.ScrollTo(Step, position: ScrollToPosition.Center, animate: false);
+						//this.CarouselView.ScrollTo(OtherStep, position: ScrollToPosition.Center, animate: false);
+						//this.CarouselView.ScrollTo(Step, position: ScrollToPosition.Center, animate: false);
 						this.uiSerializer.BeginInvokeOnMainThread(() => RegistrationViewModel.UnMuteStepSync());
 					}
 				}
